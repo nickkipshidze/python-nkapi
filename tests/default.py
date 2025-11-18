f7ec195d62e5918e157ecdb0881bdf64b5d75275
NickKipshidze <kipshidze.nick@gmail.com>
2025-11-18 21:49:16 +0400

    Update example default.py NKAPI usage

--- tests/default.py
+++ tests/default.py
@@ -1,3 +1,26 @@
 import nkapi
 
-print("Hello, World!")
+server = nkapi.NKServer(
+    host="0.0.0.0",
+    port=8000
+)
+
+def root(request):
+    return nkapi.NKResponse(
+        headers={"Content-Type": "application/json"},
+        data={
+            "method": request.method,
+            "path": request.path,
+            "query": request.query,
+            "headers": request.headers,
+            "body": request.body
+        }
+    )
+
+server.router.register(
+    method="GET",
+    path="/",
+    callback=root
+)
+
+server.start()
