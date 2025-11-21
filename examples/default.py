99331e2cad3929aeb291eb2832cb5113853e99ac
NickKipshidze <kipshidze.nick@gmail.com>
2025-11-21 18:36:08 +0400

    Update default usage example

--- examples/default.py
+++ examples/default.py
@@ -11,23 +11,21 @@ app = server.wsgi_app
 def root(request: nkapi.NKRequest):
     response = nkapi.NKResponse(
         headers={"Content-Type": "application/json"},
-        body={
+        body={"request": {
             "method": request.method,
             "path": request.path,
             "query": request.query,
             "headers": request.headers,
             "body": request.body
-        }
+        }}
     )
 
     # Hacky response body modification to return the response within the response body
-    response.body = "Request:\n" + response.body.decode() + "\n\nResponse:\n" + json.dumps({
+    response._body["response"] = {
         "status": response.status,
         "headers": response.headers,
         "body": response.body.decode()
-    }, indent=4)
-    response.body = response.body.encode("utf-8")
-    response.headers["Content-Length"] = str(len(response.body))
+    }
 
     return response
 
