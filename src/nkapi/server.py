92139aae813744c9eae413ae673a1f174a4de429
NickKipshidze <kipshidze.nick@gmail.com>
2025-11-24 23:32:09 +0400

    Add CORS & improve logging

--- src/nkapi/server.py
+++ src/nkapi/server.py
@@ -1,6 +1,7 @@
 import datetime
 import http.server
 import http.client
+import urllib.parse
 
 from . import __version__
 from . import utils
@@ -10,10 +11,29 @@ from .router import NKRouter
 class NKRequestHandler(http.server.BaseHTTPRequestHandler):
     server_version = f"NKAPI/{__version__}"
 
-    def __init__(self, router, debug=False, *args, **kwargs):
+    def __init__(self, router, debug, cors_origins, cors_headers, *args, **kwargs):
         self.router = router
         self.debug = debug
+        self.cors_origins = cors_origins
+        self.cors_headers = cors_headers
         super().__init__(*args, **kwargs)
+
+    def _cors_origin(self):
+        origin = self.headers.get("Origin")
+        if not origin:
+            return None
+        if "*" in self.cors_origins:
+            return "*"
+        if origin in self.cors_origins:
+            return origin
+        return None
+    
+    def _apply_cors(self, response):
+        origin = self._cors_origin()
+        if not origin:
+            return
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Vary"] = "Origin"
 
     def handle_request(self):
         request = NKRequest.from_handler(self)
@@ -25,10 +45,36 @@ class NKRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_PUT(self): self.handle_request()
     def do_DELETE(self): self.handle_request()
     def do_PATCH(self): self.handle_request()
-    def do_OPTIONS(self): self.handle_request()
     def do_HEAD(self): self.handle_request()
+    def do_OPTIONS(self):
+        origin = self._cors_origin()
+        path = urllib.parse.urlsplit(self.path).path
+        requested_method = self.headers.get("Access-Control-Request-Method")
+        requested_headers = self.headers.get("Access-Control-Request-Headers")
+
+        if origin == "*" or origin in self.cors_origins:
+            allowed_methods = self.router.allowed_methods(path)
+        else:
+            allowed_methods = []
+
+        allow_methods = ", ".join(sorted(set(allowed_methods + ["OPTIONS"])))
+        allow_headers = requested_headers or ", ".join(self.cors_headers)
+
+        response = NKResponse(
+            status=200,
+            headers={
+                "Content-Type": "text/plain",
+                "Access-Control-Allow-Origin": origin if origin else "",
+                "Access-Control-Allow-Methods": allow_methods,
+                "Access-Control-Allow-Headers": allow_headers,
+                "Vary": "Origin"
+            }
+        )
+
+        self.respond(response)
 
     def respond(self, response: NKResponse):
+        self._apply_cors(response)
         self.send_response(response.status)
         body = response.body
         for header, value in response.headers.items():
@@ -39,9 +85,9 @@ class NKRequestHandler(http.server.BaseHTTPRequestHandler):
     def log_message(self, format, *args):        
         timestamp = datetime.datetime.now().strftime("%I:%M:%S %p %m/%d/%Y")
         
-        method = getattr(self, "command", None)
-        path = getattr(self, "path", None)
-        version = getattr(self, "request_version", None)
+        method = getattr(self, "command", None) or "-"
+        path = getattr(self, "path", None) or "-"
+        version = getattr(self, "request_version", None) or "-"
         status = args[1] if len(args) > 1 else "-"
 
         a = utils.ANSI
@@ -55,14 +101,17 @@ class NKRequestHandler(http.server.BaseHTTPRequestHandler):
         )
 
 class NKServer:
-    def __init__(self, host="127.0.0.1", port=8000, debug=True):
+    def __init__(self, host="127.0.0.1", port=8000, debug=True, cors_origins=None, cors_headers=None):
         self.host = host
         self.port = port if port != 0 else utils.get_free_port(self.host)
         self.debug = bool(debug)
 
+        self.cors_origins = cors_origins or ["*"]
+        self.cors_headers = cors_headers or ["Content-Type", "Authorization"]
+
         self.router = NKRouter(debug=self.debug)
         self.handler = lambda *args, **kwargs: NKRequestHandler(
-            self.router, self.debug, *args, **kwargs
+            self.router, self.debug, self.cors_origins, self.cors_headers, *args, **kwargs
         )
 
     @property
